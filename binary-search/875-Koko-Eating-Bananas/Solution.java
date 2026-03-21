512a08bc69e9b77a68cef58fa0f98319982bb4c2
Kushan Shah <143536915+Kushan-shah@users.noreply.github.com>
2026-03-21 23:08:58 +0530

    Added LC 875 Koko Eating Bananas (Binary Search on Answer Space)

---
+++ binary-search/875-Koko-Eating-Bananas/Solution.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int low = 1;
+        int high = 0;
+
+        for(int i = 0 ; i < piles.length ; i++){
+            if(piles[i] > high) high = piles[i];
+        }
+
+        int ans = high;
+
+        while(low <= high){
+
+            int mid = low + (high - low) / 2;
+
+            if( canFinish(piles , h , mid) ){
+
+                ans = mid;
+                high = mid - 1;
+
+            }else{
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+
+    private boolean canFinish(int[] piles , int h , int speed){
+
+        long hours = 0;
+
+        for(int i = 0 ; i < piles.length ; i++){
+
+            hours += (piles[i] + speed - 1) / speed;
+
+            if(hours > h) return false;
+        }
+
+        return true;
+    }
+}

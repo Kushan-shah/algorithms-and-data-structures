65f156739f2a34b8845b12086f8e5fe653d3a0ff
Kushan Shah <143536915+Kushan-shah@users.noreply.github.com>
2026-03-22 21:29:38 +0530

    feat(binary-search): find first and last occurrence using lower and upper bound

---
+++ binary-search/LC 34 - Find First and Last Position/Solution.java
@@ -0,0 +1,40 @@
+class FirstAndLastPosition {
+
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{first(nums, target), last(nums, target)};
+    }
+
+    private int first(int[] nums, int target) {
+        int low = 0, high = nums.length - 1, ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] >= target) {
+                if (nums[mid] == target) ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int last(int[] nums, int target) {
+        int low = 0, high = nums.length - 1, ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] <= target) {
+                if (nums[mid] == target) ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}

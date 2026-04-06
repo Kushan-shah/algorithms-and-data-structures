7f94b32268ea21870f4c9da96c70c419fa4dfdaf
Kushan Shah <143536915+Kushan-shah@users.noreply.github.com>
2026-04-06 23:53:36 +0530

    feat(simulation): add LC 874 - Walking Robot Simulation using direction tracking and obstacle set

---
+++ string/874-Walking-Robot-Simulation/Solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+
+        Set<String> set = new HashSet<>();
+
+        for (int[] o : obstacles) {
+            set.add(o[0] + "," + o[1]);
+        }
+
+        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int d = 0;
+
+        int x = 0, y = 0;
+        int res = 0;
+
+        for (int cmd : commands) {
+
+            if (cmd == -1) {
+                d = (d + 1) % 4;
+            } else if (cmd == -2) {
+                d = (d + 3) % 4;
+            } else {
+
+                for (int i = 0; i < cmd; i++) {
+
+                    int nx = x + dirs[d][0];
+                    int ny = y + dirs[d][1];
+
+                    if (set.contains(nx + "," + ny)) break;
+
+                    x = nx;
+                    y = ny;
+
+                    res = Math.max(res, x * x + y * y);
+                }
+            }
+        }
+
+        return res;
+    }
+}

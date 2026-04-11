017dbebea1f8a196046f2856593951f73f52983b
Kushan Shah <143536915+Kushan-shah@users.noreply.github.com>
2026-04-11 19:23:03 +0530

    feat(tree): add LC 124 - Binary Tree Maximum Path Sum using optimized DFS

---
+++ tree/124-Binary-Tree-Maximum-Path-Sum/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    int res = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    int dfs(TreeNode node) {
+
+        if (node == null) return 0;
+
+        int left = Math.max(0, dfs(node.left));
+        int right = Math.max(0, dfs(node.right));
+
+        res = Math.max(res, node.val + left + right);
+
+        return node.val + Math.max(left, right);
+    }
+}

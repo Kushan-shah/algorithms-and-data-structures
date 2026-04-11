b615f61e0a34fa263f1f540da0f6733e4ec22c35
Kushan Shah <143536915+Kushan-shah@users.noreply.github.com>
2026-04-11 19:09:28 +0530

    feat(array): add LC 1590 - Make Sum Divisible by P using optimized prefix modulo hashmap

---
+++ arrays/1590-Make-Sum-Divisible-by-P/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+
+        long total = 0;
+
+        for (int num : nums) total += num;
+
+        int rem = (int)(total % p);
+        if (rem == 0) return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int prefix = 0;
+        int res = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            prefix = (prefix + nums[i]) % p;
+
+            int need = (prefix - rem + p) % p;
+
+            if (map.containsKey(need)) {
+                res = Math.min(res, i - map.get(need));
+            }
+
+            map.put(prefix, i);
+        }
+
+        return res == nums.length ? -1 : res;
+    }
+}
